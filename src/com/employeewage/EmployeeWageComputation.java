4c9da7b76929a7bb17d7b05ce14700cb97b5bbda
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-08-23 13:59:51 +0530

    Checked whether employee is present or absent as in UC1

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -4,6 +4,17 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Computation Problem");
+		
+		int IS_FULL_TIME=1;
+		
+		int empCheck = (int) (Math.floor(Math.random()*10)%2);
+		
+		if(empCheck == IS_FULL_TIME)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
+		
+		
 
 	}
 
