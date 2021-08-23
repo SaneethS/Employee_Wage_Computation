2a8a4fcc9fe4b565e0ae5972b449f82b263be78e
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-08-23 14:11:25 +0530

    Added part time employee and calculated wage as in UC3

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -6,15 +6,19 @@ public class EmployeeWageComputation {
 		System.out.println("Welcome to Employee Computation Problem");
 		
 		int IS_FULL_TIME=1;
+		int IS_PART_TIME=2;
 		int WAGE_PER_HOUR=20;
 		int empHrs=0;
 		int empWage=0;
 		
-		int empCheck = (int) (Math.floor(Math.random()*10)%2);
+		int empCheck = (int) (Math.floor(Math.random()*10)%3);
 		
 		if(empCheck == IS_FULL_TIME) {
 			System.out.println("Employee is present");
 			empHrs=8;
+		}else if(empCheck == IS_PART_TIME) {
+			System.out.println("Employee is present but part time");
+			empHrs=4;
 		}
 		else {
 			System.out.println("Employee is absent");
