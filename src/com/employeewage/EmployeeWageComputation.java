8d4653dd2359d3ceb22fff3fac103d14e2b18c1b
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-08-23 14:54:17 +0530

    Calculted Wage for particular hours and days in a month as in UC6

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -8,9 +8,12 @@ public class EmployeeWageComputation {
 		final int IS_FULL_TIME=1;
 		final int IS_PART_TIME=2;
 		final int WORKING_DAYS_MONTH=20;
+		final int WORKING_HOURS_MONTH=100;
 		int WAGE_PER_HOUR=20;
 		int empHrs=0;
 		int monthlyWage=0;
+		int totalHrs=0;
+		int totalDays=0;
 		
 		int empCheck = (int) (Math.floor(Math.random()*10)%3);
 		
@@ -42,6 +45,25 @@ public class EmployeeWageComputation {
 			
 		}
 		System.out.println("Wage of Employee per Month: "+monthlyWage);
+		
+		while(totalHrs<WORKING_HOURS_MONTH && totalDays<WORKING_DAYS_MONTH) {
+			totalDays++;
+			switch(empCheck) {
+			case IS_FULL_TIME: empHrs=8;
+						break;
+			case IS_PART_TIME: empHrs=4;
+						break;
+			default: empHrs=0;
+	
+			}
+			totalHrs += empHrs;
+			System.out.println("Day "+totalDays+" Hours:"+totalHrs);
+			
+		}
+		
+		int totalWage = totalHrs * WAGE_PER_HOUR;
+		System.out.println("Total Wage of Employee: "+totalWage);
+		
 		
 
 	}
