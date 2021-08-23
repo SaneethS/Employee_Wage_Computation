f0aa1ecc053c3d294c51a6863361c03ce537e8e2
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-08-23 14:17:36 +0530

    Calculated wage using switch-case statement

--- src/com/employeewage/EmployeeWageComputation.java
+++ src/com/employeewage/EmployeeWageComputation.java
@@ -5,8 +5,8 @@ public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Computation Problem");
 		
-		int IS_FULL_TIME=1;
-		int IS_PART_TIME=2;
+		final int IS_FULL_TIME=1;
+		final int IS_PART_TIME=2;
 		int WAGE_PER_HOUR=20;
 		int empHrs=0;
 		int empWage=0;
@@ -27,6 +27,17 @@ public class EmployeeWageComputation {
 		
 		int dailyWage = empHrs * WAGE_PER_HOUR;
 		System.out.println("Daily Wage of Employee : "+dailyWage);
+		
+		switch(empCheck) {
+		case IS_FULL_TIME: empHrs=8;
+							break;
+		case IS_PART_TIME: empHrs=4;
+							break;
+		default: empHrs=0;
+		
+		}
+		int dailyWageSwitch = empHrs * WAGE_PER_HOUR;
+		System.out.println("Daily Wage of Employee using Switch: "+dailyWageSwitch);
 		
 
 	}
