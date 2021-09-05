33ffbfb932a8bbd179cee6dc1de92007894ab6c0
Saneeth S <saneeths@125AMBBLR-Chethan-RV.local>
2021-09-05 19:33:31 +0530

    UC10 has been created where different companies are added and its wage is computed

---
+++ src/com/employeewage/CompEmpWage.java
@@ -0,0 +1,28 @@
+package com.employeewage;
+
+public class CompEmpWage {
+	
+	public String companyName;
+	public int wagePerHour;
+	public int workingDaysPerMonth;
+	public int workingHoursPerMonth;
+	public int totalEmpWage;
+
+	CompEmpWage(String companyName,int wagePerHour, int workingDaysPerMonth, int workingHoursPerMonth){
+		this.companyName = companyName;
+		this.wagePerHour = wagePerHour;
+		this.workingDaysPerMonth = workingDaysPerMonth;
+		this.workingHoursPerMonth = workingHoursPerMonth;
+	}
+	
+	public void setTotalEmpWage(int totalEmpWage) {
+		this.totalEmpWage = totalEmpWage;
+	}
+
+	@Override
+	public String toString() {
+		return "Total Employee wage in company named "+companyName+" is = "+totalEmpWage+"\n\n";
+	}
+	
+	
+}
